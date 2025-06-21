64784a9e345a32b32a504ee480ecdc854da1f8cc
Prakhar Rastogi <67405869+prakhar-cse@users.noreply.github.com>
2025-06-21 23:46:34 +0530

    Create 2_ValidAnagram.java

---
+++ Arrays and Hashing/2_ValidAnagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+        /// sorting
+        // char[] sa = s.toCharArray();
+        // char[] ta = t.toCharArray();
+        // Arrays.sort(sa);
+        // Arrays.sort(ta);
+
+        // for(int i=0;i<sa.length;i++){
+        //     if(sa[i] != ta[i]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        /// hashmap
+        
+
+        /// fq array
+    }
+}
